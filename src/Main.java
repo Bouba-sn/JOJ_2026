5ce3d2ef2df8192547635f743591954e5bec6749
Bouba-sn <carb91910@gmail.com>
2026-08-18 18:39:17 +0000

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import dao.Database;
 import service.IMenu;
 import service.IMenuImple;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Démarrage de l'application JOJ Dakar 2026...");
@@ -11,7 +10,6 @@ public class Main {
         }
         IMenu menu = new IMenuImple();
         menu.afficherMenuPrincipal();
-
         Database.closeConnection();
         System.out.println("Application terminée.");
     }
